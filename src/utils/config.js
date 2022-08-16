24f86b892c610e6b9cb96cde2e884e7fb84c9d31
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-16 18:11:50 +0200

    update

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,7 @@ import jwt_decode from "jwt-decode";
 
 var token =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8va2xhYmF0dGVuZGVlcy5oZXJva3VhcHAuY29tL2FwaS9sb2dpbi9Mb2dpbkluZm8iLCJpYXQiOjE2NjA2MzgyNjEsImV4cCI6MTY2MDY0MTg2MSwibmJmIjoxNjYwNjM4MjYxLCJqdGkiOiJwVWlLbjh6dGF3U3BueXppIiwic3ViIjoiOSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjciLCJpZCI6Im1lbWJlciIsImVtYWlsIjoibnR3YXJpQGdtYWlsLmNvbSJ9.9JVUV0CStFy2ItV8Xoy1v7umximAfUkZo7R1Vt1VjiI";
+var key = "";
 var decoded = jwt_decode(token);
 
 console.log(decoded);
