d0caf62717da5833daaa8117da0d3119ea22dc82
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-17 20:00:45 +0200

    update

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigation from "./src/navigation/rootNavigator";
-import { AuthProvider } from "./src/context/AuthContext";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 import { LogBox } from "react-native";
