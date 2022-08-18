e2aae32d117168f91bb7742dfb912ef551e27f1a
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-18 20:09:05 +0200

    update

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
+import ProgramReducer from "./ProgramReducer";
 export default combineReducers({
   Auth: AuthReducer,
+  Program: ProgramReducer,
 });
