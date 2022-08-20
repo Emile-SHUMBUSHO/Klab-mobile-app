69776c719dc52ce71fa846b6c7af5669666dd391
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-20 22:01:18 +0200

    update

--- src/features/terms&conditions/screens/terms&conditions.js
+++ src/features/terms&conditions/screens/terms&conditions.js
@@ -16,31 +16,30 @@ const TermsAndConditionsScreen = (props) => {
             props.navigation.navigate("signUp");
           }}
         >
-          <Entypo name="chevron-left" size={24} color="black" left={20} />
+          <Entypo name="chevron-left" size={24} color="white" left={20} />
         </TouchableOpacity>
-        <Text style={{ color: "black", fontSize: 18, left: 60 }}>
+        <Text style={{ color: "#fff", fontSize: 18, left: 60 }}>
           Terms and Conditions
         </Text>
       </View>
-      <View>
-        <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
-          <View style={styles.termsContent}>
-            <Text>
-              Terms of service also known as terms of use and terms and
-              conditions, commonly abbreviated as TOS or ToS, ToU or T&C are the
-              legal agreements between a service provider and a person who wants
-              to use that service. The person must agree to abide by the terms
-              of service in order to use the offered service.1 Terms of service
-              can also be merely a disclaimer, especially regarding the use of
-              websites. Vague language and lengthy sentences used in the terms
-              of use have brought concerns on customer privacy and raised public
-              awareness in many ways.
-            </Text>
-            <Text>Section 1: Who we are ?</Text>
-            <Text>Section 2: Definitions</Text>
-          </View>
-        </ScrollView>
-      </View>
+
+      <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
+        <View style={styles.termsContent}>
+          <Text>
+            Terms of service also known as terms of use and terms and
+            conditions, commonly abbreviated as TOS or ToS, ToU or T&C are the
+            legal agreements between a service provider and a person who wants
+            to use that service. The person must agree to abide by the terms of
+            service in order to use the offered service.1 Terms of service can
+            also be merely a disclaimer, especially regarding the use of
+            websites. Vague language and lengthy sentences used in the terms of
+            use have brought concerns on customer privacy and raised public
+            awareness in many ways.
+          </Text>
+          <Text>Section 1: Who we are ?</Text>
+          <Text>Section 2: Definitions</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -51,8 +50,7 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     width: "100%",
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "black",
   },
   top: {
     height: "15%",
