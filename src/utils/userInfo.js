8a46dffbc71449517a8aaaea9aafc2c4c20f7793
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-21 17:01:51 +0200

    update

--- src/utils/userInfo.js
+++ src/utils/userInfo.js
@@ -3,7 +3,6 @@ import jwt_decode from "jwt-decode";
 export const UserInfo = async (token) => {
   try {
     const userInfo = await jwt_decode(token);
-
     return userInfo;
   } catch (err) {
     console.log(err);
