493b326bd3c057b8680ff34fb9343b6e6674cb74
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-25 17:39:17 +0200

    update

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,2 +1,3 @@
 export * from "./auth.actions.js";
 export * from "./programs.actions.js";
+export * from "./register.child.js";
--- src/redux/actions/register.child.js
+++ src/redux/actions/register.child.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "../../config";
 
-export const Register = (
+export const register = (
   firstName,
   lastName,
   gender,
