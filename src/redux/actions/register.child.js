712c04f53c7faa3a70dcd681b4449cda941e5144
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-25 13:51:43 +0200

    update

--- src/redux/actions/register.child.js
+++ src/redux/actions/register.child.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-import jwt_decode from "jwt-decode";
 import { BASE_URL } from "../../config";
-export const Init = () => {
-  return async (dispatch) => {};
-};
 
 export const Register = (name, email, role, password) => {
   return async (dispatch) => {
