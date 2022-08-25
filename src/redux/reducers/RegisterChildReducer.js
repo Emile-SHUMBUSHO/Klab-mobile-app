7f002a3f948aa7af1882971d54b049efe1c6c1c2
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-25 14:16:08 +0200

    update

--- src/redux/reducers/RegisterChildReducer.js
+++ src/redux/reducers/RegisterChildReducer.js
@@ -3,4 +3,21 @@ const initialState = {
   error: null,
 };
 
-export default (state = initialState, action) => {};
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "":
+      return {
+        ...state,
+        loading: false,
+        error: null,
+      };
+    case "":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
