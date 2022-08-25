1ddebd86c535d48eabd8a0c74a735fbe9304e013
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-25 10:26:06 +0200

    update

--- src/components/homeScreen/programs.js
+++ src/components/homeScreen/programs.js
@@ -1,242 +1,91 @@
-import {View, ImageBackground, Text } from "react-native";
-const Programs = () =>{
-    return(
-        <View style={styles.content}>
-        <ImageBackground
-          source={require("../../../assets/s2.jpg")}
-          borderTopRightRadius={20}
-          borderTopLeftRadius={20}
+import { View, ImageBackground, Text, StyleSheet } from "react-native";
+export const Programs = () => {
+  return (
+    <View style={styles.content}>
+      <ImageBackground
+        source={require("../../../assets/s2.jpg")}
+        borderTopRightRadius={20}
+        borderTopLeftRadius={20}
+      >
+        <View style={{ height: 200 }} />
+      </ImageBackground>
+      <View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
         >
-          <View style={{ height: 200 }} />
-        </ImageBackground>
-        <View>
+          <Text style={{ margin: 10 }}>Klab start up</Text>
           <View
             style={{
               flexDirection: "row",
               alignItems: "center",
               justifyContent: "space-between",
+              margin: 10,
             }}
           >
-            <Text style={{ margin: 10 }}>Klab start up</Text>
-            <View
+            <TouchableOpacity style={{ right: 10 }}>
+              <AntDesign name="hearto" size={24} color="black" />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => {
+                props.navigation.navigate("join");
+              }}
               style={{
-                flexDirection: "row",
+                width: 50,
+                height: 25,
+                backgroundColor: "black",
                 alignItems: "center",
-                justifyContent: "space-between",
-                margin: 10,
+                justifyContent: "center",
+                flexDirection: "row",
+                borderRadius: 25,
               }}
             >
-              <TouchableOpacity style={{ right: 10 }}>
-                <AntDesign name="hearto" size={24} color="black" />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                onPress={() => {
-                  props.navigation.navigate("join");
-                }}
-                style={{
-                  width: 50,
-                  height: 25,
-                  backgroundColor: "black",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row",
-                  borderRadius: 25,
-                }}
-              >
-                <Text style={{ color: "white" }}>Join</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View>
-            <Text style={{ margin: 10 }}>
-              kLab Rwanda is hosting an info session to present to the
-              entrepreneurs about alibaba's ecosystem and training programs
-              that they have in place. The participants can be tech
-              entrepreneurs. they are not only focusing on ecommerce...
-              <TouchableOpacity>
-                <Text>Read More</Text>
-              </TouchableOpacity>
-            </Text>
+              <Text style={{ color: "white" }}>Join</Text>
+            </TouchableOpacity>
           </View>
         </View>
-      </View>
-      <View style={styles.content}>
-        <ImageBackground
-          source={require("../../../assets/s1.jpg")}
-          borderTopRightRadius={20}
-          borderTopLeftRadius={20}
-        >
-          <View style={{ height: 200 }} />
-        </ImageBackground>
         <View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ margin: 10 }}>Klab Events</Text>
-
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                margin: 10,
-              }}
-            >
-              <TouchableOpacity style={{ right: 10 }}>
-                <AntDesign name="hearto" size={24} color="black" />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{
-                  width: 50,
-                  height: 25,
-                  backgroundColor: "black",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row",
-                  borderRadius: 25,
-                }}
-              >
-                <Text style={{ color: "white" }}>Join</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View>
-            <Text style={{ margin: 10 }}>
-              kLab Rwanda is hosting an info session to present to the
-              entrepreneurs about alibaba's ecosystem and training programs
-              that they have in place. The participants can be tech
-              entrepreneurs. they are not only focusing on ecommerce...
-              <TouchableOpacity>
-                <Text>Read More</Text>
-              </TouchableOpacity>
-            </Text>
-          </View>
+          <Text style={{ margin: 10 }}>
+            kLab Rwanda is hosting an info session to present to the
+            entrepreneurs about alibaba's ecosystem and training programs that
+            they have in place. The participants can be tech entrepreneurs. they
+            are not only focusing on ecommerce...
+            <TouchableOpacity>
+              <Text>Read More</Text>
+            </TouchableOpacity>
+          </Text>
         </View>
       </View>
-      <View style={styles.content}>
-        <ImageBackground
-          source={require("../../../assets/s3.jpg")}
-          borderTopRightRadius={20}
-          borderTopLeftRadius={20}
-        >
-          <View style={{ height: 200 }} />
-        </ImageBackground>
-        <View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ margin: 10 }}>Future Coders</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                margin: 10,
-              }}
-            >
-              <TouchableOpacity style={{ right: 10 }}>
-                <AntDesign name="hearto" size={24} color="black" />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                onPress={() => {
-                  props.navigation.navigate("join");
-                }}
-                style={{
-                  width: 50,
-                  height: 25,
-                  backgroundColor: "black",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row",
-                  borderRadius: 25,
-                }}
-              >
-                <Text style={{ color: "white" }}>Join</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View>
-            <Text style={{ margin: 10 }}>
-              kLab Rwanda is hosting an info session to present to the
-              entrepreneurs about alibaba's ecosystem and training programs
-              that they have in place. The participants can be tech
-              entrepreneurs. they are not only focusing on ecommerce...
-              <TouchableOpacity>
-                <Text>Read More</Text>
-              </TouchableOpacity>
-            </Text>
-          </View>
-        </View>
-      </View>
-      <View style={styles.content}>
-        <ImageBackground
-          source={require("../../../assets/s4.jpg")}
-          borderTopRightRadius={20}
-          borderTopLeftRadius={20}
-        >
-          <View style={{ height: 200 }} />
-        </ImageBackground>
-        <View>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text style={{ margin: 10 }}>Tech Up Skill</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
-                margin: 10,
-              }}
-            >
-              <TouchableOpacity style={{ right: 10 }}>
-                <AntDesign name="hearto" size={24} color="black" />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{
-                  width: 50,
-                  height: 25,
-                  backgroundColor: "black",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row",
-                  borderRadius: 25,
-                }}
-              >
-                <Text style={{ color: "white" }}>Join</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View>
-            <Text style={{ margin: 10 }}>
-              kLab Rwanda is hosting an info session to present to the
-              entrepreneurs about alibaba's ecosystem and training programs
-              that they have in place. The participants can be tech
-              entrepreneurs. they are not only focusing on ecommerce...
-              <TouchableOpacity>
-                <Text>Read More</Text>
-              </TouchableOpacity>
-            </Text>
-          </View>
-        </View>
-      </View>
-    );
+    </View>
+  );
 };
 
-export default Programs;
+const styles = StyleSheet.create({
+  top: {
+    height: "15%",
+    width: "100%",
+  },
+  content: {
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+    margin: 20,
+  },
+  container: {
+    backgroundColor: "black",
+    height: "100%",
+    width: "100%",
+  },
+});
