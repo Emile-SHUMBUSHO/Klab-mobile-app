1f91ce627e856612b92fb2cc4fc7a5bee828d713
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-25 09:30:55 +0200

    update

--- src/redux/actions/programs.actions.js
+++ src/redux/actions/programs.actions.js
@@ -31,3 +31,33 @@ export const fetchChildProgram = () => {
     }
   };
 };
+
+export const fetchAllProgram = () => {
+  return async (dispatch) => {
+    dispatch({ type: "FETCHALLPROGRAM" });
+    let token = await AsyncStorage.getItem("token");
+    console.log("token from async storage");
+    console.log(token);
+    try {
+      await axios({
+        method: "GET",
+        url: `${BASE_URL}/futurecoders/programInformation`,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          accept: "application/json",
+        },
+      }).then((responseData) => {
+        const { data } = responseData;
+        console.log("program data");
+        console.log(data.data);
+        dispatch({
+          type: "FETCHPROGRAM SUCCESS",
+          payload: data.data,
+        });
+      });
+    } catch (err) {
+      console.log(err);
+      dispatch({ type: "FETCHPROGRAM FAILED", payload: err.message });
+    }
+  };
+};
