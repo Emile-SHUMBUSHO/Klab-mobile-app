e5d66103e9ef2a4bd886346220a15bfdc33cd0fb
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-26 17:45:20 +0200

    update

--- src/features/registerChild/screens/index.js
+++ src/features/registerChild/screens/index.js
@@ -17,7 +17,7 @@ import { register } from "../../../redux/actions/register.child";
 import DatePicker from "react-native-date-picker";
 import DropDownPicker from "react-native-dropdown-picker";
 const RegisterScreen = ({ route, navigation }) => {
-  const [date, setDate] = useState(new Date(1598051730000));
+  const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
 
