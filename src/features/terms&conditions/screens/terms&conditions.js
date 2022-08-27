598800a1b5794c09f7892328a3d751f39a95690a
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-27 22:48:07 +0200

    update

--- src/features/terms&conditions/screens/terms&conditions.js
+++ src/features/terms&conditions/screens/terms&conditions.js
@@ -30,7 +30,7 @@ const TermsAndConditionsScreen = (props) => {
             commonly abbreviated as TOS oToS, ToU orT& are the legal agreements
             between a service rovider and a person wh wants to use that service.
             The personmustagre to aide by the terms of service in order to se
-            the offered srvice1 Trms of service can also be merely a disclaimer,
+            the offered srvice1 Trms of service can also b merely a disclaimer,
             especially regarding the use of websites. Vague language and lengthy
             sentences ued n the terms of use have brought concerns on customer
             privacy and raised public awa
