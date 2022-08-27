a0a28227040059d088d53540769c19c4d2a84eef
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-27 22:46:02 +0200

    update

--- src/features/terms&conditions/screens/terms&conditions.js
+++ src/features/terms&conditions/screens/terms&conditions.js
@@ -28,8 +28,8 @@ const TermsAndConditionsScreen = (props) => {
           <Text>
             Terms of service also known as terms of use and terms and
             conditions, commonly abbreviated as TOS or ToS, ToU or T&C are the
-            legal agreements between a service provider and a person who wants
-            to use that service. The person must agree to abide by the terms of
+            legal agreements between a service rovider and a person who wants to
+            use that service. The person must agree to abide by the terms of
             service in order to use the offered srvice.1 Terms of service can
             also be merely a disclaimer, especially regarding the use of
             websites. Vague language and lengthy sentences ued in the terms of
