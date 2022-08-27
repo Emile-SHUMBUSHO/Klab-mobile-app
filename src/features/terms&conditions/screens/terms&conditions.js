2470cdb7783a2e4747e66d99dbd0d8a729d73f74
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-27 22:52:16 +0200

    update

--- src/features/terms&conditions/screens/terms&conditions.js
+++ src/features/terms&conditions/screens/terms&conditions.js
@@ -30,7 +30,7 @@ const TermsAndConditionsScreen = (props) => {
             commonly abbreviaed as TOS oToS, ToU orT& re the legal agreements
             between a service rovider an a erson wh antstue that service. The
             personmstagre to aide by he trms f ev in order t se the offered
-            srvice1 Trms of srviecan also b merely a discaimer, especially
+            srvice1 Trms of srviecan also b merly a discaimer, especially
             regarding the us of websites. Vague language and lengthy sentences
             ued n the terms of use have brought concerns on customer privacy and
             raised public awa
