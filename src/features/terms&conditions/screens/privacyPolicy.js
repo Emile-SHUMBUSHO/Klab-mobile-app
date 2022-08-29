0498efbea57c7ffe24c180a8cbdeb21f0dca95bb
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-29 23:23:43 +0200

    update

--- src/features/terms&conditions/screens/privacyPolicy.js
+++ src/features/terms&conditions/screens/privacyPolicy.js
@@ -37,7 +37,7 @@ const PrivacyPolicyScreen = (props) => {
               profile picture, photos or avatar, gendr, age, ethnicity,
               religion, marital status, educational level, inteests, hobbie
               precise location-based information, e-mail and maiing addesse,
-              phone numbers, devic I numbers anpasswords when y creae an account
+              phone numbers, devic I numbers anpsswords when y creae an account
               for our Services “Boost Account, an any othe information you
               directly give us through the Services incluing, but not limited to
               messages that you send throu
