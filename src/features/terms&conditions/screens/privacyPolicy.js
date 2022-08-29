5ee12f7134db4cffcf25101b12360855087869ea
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-29 23:23:51 +0200

    update

--- src/features/terms&conditions/screens/privacyPolicy.js
+++ src/features/terms&conditions/screens/privacyPolicy.js
@@ -36,7 +36,7 @@ const PrivacyPolicyScreen = (props) => {
               Personal Information from you, such as your first and last name,
               profile picture, photos or avatar, gendr, age, ethnicity,
               religion, marital status, educational level, inteests, hobbie
-              precise location-based information, e-mail and maiing addesse,
+              precise location-based nformation, e-mail and maiing addesse,
               phone numbers, devic I numbers anpsswords when y creae an account
               for our Services “Boost Account, an any othe information you
               directly give us through the Services incluing, but not limited to
