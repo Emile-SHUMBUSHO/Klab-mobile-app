ce17f86a25ca56f79512c589c2fc76bb5eba2577
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-08-31 09:39:56 +0200

    update

---
+++ src/features/klab-members/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const KlabMembersHomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to Klab Members</Text>
+    </View>
+  );
+};
+
+export default KlabMembersHomeScreen;
