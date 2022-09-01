0647bb0dfd093c4956d775bc841c75348f5df57b
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-09-01 10:53:58 +0200

    update

--- src/features/auth/sign-up/index.js
+++ src/features/auth/sign-up/index.js
@@ -20,45 +20,6 @@ import Loader from "../../../components/loader";
 import { Register } from "../../../redux/actions";
 import { useDispatch } from "react-redux";
 
-const ModalPoup = ({ visible, children }) => {
-  const [showModal, setShowModal] = useState(visible);
-  const scaleValue = useRef(new Animated.Value(0)).current;
-  useEffect(() => {
-    toggleModal();
-  }, [visible]);
-  const toggleModal = () => {
-    if (visible) {
-      setShowModal(true);
-      Animated.spring(scaleValue, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    } else {
-      setTimeout(() => setShowModal(false), 200);
-      Animated.timing(scaleValue, {
-        toValue: 0,
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    }
-  };
-  return (
-    <Modal transparent visible={showModal}>
-      <View style={styles.modalBackGround}>
-        <Animated.View
-          style={[
-            styles.modalContainer,
-            { transform: [{ scale: scaleValue }] },
-          ]}
-        >
-          {children}
-        </Animated.View>
-      </View>
-    </Modal>
-  );
-};
-
 const SignUpScreen = (props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [visible, setVisible] = useState(false);
