b838ef4a33cdafa63c7c2599f6b57e174d6835e6
Emile-SHUMBUSHO <shumbushoemilef@gmail.com>
2022-09-02 18:26:46 +0200

    update

--- src/components/homeScreen/programs.js
+++ src/components/homeScreen/programs.js
@@ -77,8 +77,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
+
     margin: 20,
   },
 });
